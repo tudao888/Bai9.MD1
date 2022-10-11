f7822cb04e6c089ad00349854abe212bc88e5965
tudao888 <daotu2106@gmail.com>
2022-10-11 17:27:06 +0700

    sualan2

--- trongthang.js
+++ trongthang.js
@@ -1,31 +1,42 @@
 function kiemtra() {
     let month = +document.getElementById("sothang").value;
     switch (month) {
-        case 1: document.getElementById("result").innerHTML = "Tháng 1 có 30 ngày";
+        // case 1:
+        // document.getElementById("result").innerHTML = "Tháng 1 có 30 ngày";
+        //     break;
+        // case 2: document.getElementById("result").innerHTML = "Tháng 2 có 28 ngày";
+        //     break;
+        // case 3: document.getElementById("result").innerHTML = "Tháng 3 có 31 ngày";
+        //     break;
+        // case 4: document.getElementById("result").innerHTML = "Tháng 4 có 30 ngày";
+        //     break;
+        // case 5: document.getElementById("result").innerHTML = "Tháng 5 có 31 ngày";
+        //     break;
+        // case 6: document.getElementById("result").innerHTML = "Tháng 6 có 29 ngày";
+        //     break;
+        // case 7: document.getElementById("result").innerHTML = "Tháng 7 có 30 ngày";
+        //     break;
+        // case 8: document.getElementById("result").innerHTML = "Tháng 8 có 31 ngày";
+        //     break;
+        // case 9: document.getElementById("result").innerHTML = "Tháng 9 có 30 ngày";
+        //     break;
+        // case 10: document.getElementById("result").innerHTML = "Tháng 10 có 29 ngày";
+        //     break;
+        // case 11: document.getElementById("result").innerHTML = "Tháng 11 có 31 ngày";
+        //     break;
+        // case 12: document.getElementById("result").innerHTML = "Tháng 12 có 30 ngày";
+        //     break;
+        case 1:
+        case 4:
+        case 9:
+        case 7:
+            document.getElementById('result').innerHTML = 'Thang ' + month + ' co 30 ngay'
             break;
-        case 2: document.getElementById("result").innerHTML = "Tháng 2 có 28 ngày";
+        case 8:
+        case 5:
+            document.getElementById('result').innerHTML = 'Thang ' + month + ' co 31 ngay'
             break;
-        case 3: document.getElementById("result").innerHTML = "Tháng 3 có 31 ngày";
-            break;
-        case 4: document.getElementById("result").innerHTML = "Tháng 4 có 30 ngày";
-            break;
-        case 5: document.getElementById("result").innerHTML = "Tháng 5 có 31 ngày";
-            break;
-        case 6: document.getElementById("result").innerHTML = "Tháng 6 có 29 ngày";
-            break;
-        case 7: document.getElementById("result").innerHTML = "Tháng 7 có 30 ngày";
-            break;
-        case 8: document.getElementById("result").innerHTML = "Tháng 8 có 31 ngày";
-            break;
-        case 9: document.getElementById("result").innerHTML = "Tháng 9 có 30 ngày";
-            break;
-        case 10: document.getElementById("result").innerHTML = "Tháng 10 có 29 ngày";
-            break;
-        case 11: document.getElementById("result").innerHTML = "Tháng 11 có 31 ngày";
-            break;
-        case 12: document.getElementById("result").innerHTML = "Tháng 12 có 30 ngày";
-            break;
-
         default: document.getElementById("result").innerHTML = "khong co thang nay"
+            break;
 }
 }
